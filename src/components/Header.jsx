fe39b3faa6502c01114770318d81760acece48d7
Писанко Александр <avpisanko@mail.ru>
2023-11-29 19:22:30 +0300

    123

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,11 +9,9 @@ function Header() {
             <div><img src={logo} alt="" className='logo'/></div>
             <div className='Info'>
                 <div><Link to='/' className='infoTxt'>На главную</Link></div>
-                <div><Link to='/About' className='infoTxt'>О нас</Link></div>
+                <div><Link to='/About' className='infoTxt'>О нас!</Link></div>
             </div>
             <div><img src={settings} alt="" className='settings' /></div>
-
-            
         </div>
     )
 }
