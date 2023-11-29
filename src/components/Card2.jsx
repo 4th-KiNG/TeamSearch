4229e7f725f2d4f77b4a6a4b8d92a2f3d0329ccd
Писанко Александр <avpisanko@mail.ru>
2023-11-29 19:57:22 +0300

    123

--- src/components/Card2.jsx
+++ src/components/Card2.jsx
@@ -6,7 +6,7 @@ import './Card2.css'
         <div className="background2">
             <div className="title2">Подробная информация об игроках и командах</div>
             <div className="lineincard2"></div>
-            <div className="discription2">На нашем сайте ты сможешь увидеть подробную информацию о командах и игроках: их достижения, опыт , особенности.</div>
+            <div className="discription2">На нашем сайте ты сможешь увидеть подробную информацию о командах и игроках: их достижения, опыт, особенности.</div>
         </div>
     )
 }
