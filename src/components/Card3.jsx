8942ec1415dcdab7a651e0b9772338c3988153db
Писанко Александр <avpisanko@mail.ru>
2023-11-29 18:55:49 +0300

    123

--- src/components/Card3.jsx
+++ src/components/Card3.jsx
@@ -5,7 +5,6 @@ import './Card3.css'
     return(
         <div classname='crd3'>
         <div className="background3">
-        
             <div className="title3">Удобный пользовательский интерфейс</div>
             <div className="lineincard3"></div>
             <div className="discription3">Интерфейс нашего сервиса интуитивно понятен и удобен , что позволяет найти нужную информацию быстро и с комфортом.</div>
